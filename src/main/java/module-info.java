25e7f4375de55559e479b00c250ffd40723bc333
Tin-Trung Nguyen <hopthucuatin@gmail.com>
2021-12-15 16:58:18 +0700

    Add UI

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.github.tinplayscode.slang {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.github.tinplayscode.slang to javafx.fxml;
+    exports com.github.tinplayscode.slang;
+}
